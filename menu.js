3a93e3d9138eec9679c718befced992ab638bb45
ludo loops <ludoloops@protonmail.com>
2021-10-06 01:30:04 +0200

    refactoring

--- menu.js
+++ menu.js
@@ -41,17 +41,3 @@ module.exports.listMenu = menu => {
   const menuWithIndex = items.join('')
   return menuWithIndex
 }
-
-// module.exports.listCompetences = () => {
-//   const menu = competence
-//   const items = []
-
-//   menu.map(item => {
-//     const index = menu.indexOf(item) + 1
-//     const temp = `${index}: ${item} `
-
-//     items.push(temp)
-//   })
-//   const menuWithIndex = items.join('')
-//   return menuWithIndex
-// }
